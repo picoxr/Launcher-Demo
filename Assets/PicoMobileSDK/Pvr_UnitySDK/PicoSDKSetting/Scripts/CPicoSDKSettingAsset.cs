185c97abe8a080eeed23a8dbf4e3dad3567f5f38
Pico <46362299+picoxr@users.noreply.github.com>
2020-03-13 18:38:15 +0800

    update

--- Assets/PicoMobileSDK/Pvr_UnitySDK/PicoSDKSetting/Scripts/CPicoSDKSettingAsset.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CPicoSDKSettingAsset : ScriptableObject
-{
-    public bool IgnoreSDKSetting = false;
-}
